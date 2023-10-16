0abd78ac2aa02b78b1080371846f331e88ed7743
yanis <yanis.boussadia@etu.univ-tours.fr>
2023-10-16 09:41:05 +0200

    first_commit

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 # Bienvenue
 Programme Java qui affiche le nom de l’étudiant en utilisant la classe SimpleTesting ()
+# VSC-TP1_NomEt2
---
+++ Student.java
@@ -0,0 +1,9 @@
+class Student{
+String name;
+Student(String name){
+this.name = name;
+}
+public String getName() {
+return this.name;
+}
+}
